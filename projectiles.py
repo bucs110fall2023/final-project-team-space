e9abcc091b165ccf7b95e949aecc882d77e3f96c
Christian <capn.chirs05@gmail.com>
2023-12-01 17:15:28 -0500

    Work

---
+++ projectiles.py
@@ -0,0 +1,19 @@
+class Projectiles:
+    def __init__(self,x,y,color,radius
+        self.x = x
+        self.y = y
+        self.color = color
+        self.radius = radius
+
+                 
+    def Bolt(self)
+        pygame.draw.circle(Surface,self.color, (self.x,self.y), self.radius
+
+    bolts = []
+    while True
+        for event in pygame.event.get():
+            if event.type = pygame.QUIT:
+                break
+        for i in bolts:
+
+        if 
