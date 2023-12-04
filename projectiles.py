491a5bb6b337cda18de1add98483120b8702e3d1
Christian <capn.chirs05@gmail.com>
2023-12-04 17:45:32 -0500

    Update to ATP and Projectile

--- projectiles.py
+++ projectiles.py
@@ -1,10 +1,13 @@
 class Projectiles:
-    def __init__(self,x,y,color,radius
+    def __init__(self,x,y,color,radius, velocity):
         self.x = x
         self.y = y
         self.color = color
         self.radius = radius
-
-                 
-    def Bolt(self)
+        self.velocity = 6
+ 
+    def Bolt(self,win):
         pygame.draw.circle(win ,self.color, (self.x,self.y), self.radius)
+
+
+     
